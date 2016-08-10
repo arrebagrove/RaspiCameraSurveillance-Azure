7c47335795558be645c8352021f9a0bf4a9c6dcd
Edi Wang <Edi.Wang@outlook.com>
2016-08-10 18:27:40 +0800

    checkin azure website

---
+++ AzureWebsite/PirCamWatcher/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace PirCamWatcher.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static CloudBlobContainer BlobContainer { get; set; }
+
+        public HomeController()
+        {
+            BlobContainer = GetBlobContainer();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetImages()
+        {
+            var listBlobProperties = (from item in BlobContainer.ListBlobs(null, false)
+                                      where item.GetType() == typeof(CloudBlockBlob)
+                                      select (CloudBlockBlob)item
+                                      into blob
+                                      select new Models.PirCamBlobProperties(blob.Properties.LastModified, blob.Uri))
+                                     .OrderByDescending(p => p.DateModified)
+                                     .ToList();
+
+            return Json(listBlobProperties, JsonRequestBehavior.AllowGet);
+        }
+
+        private static CloudBlobContainer GetBlobContainer()
+        {
+            string connectionString = WebConfigurationManager.AppSettings["StorageConnectionString"];
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container =
+                blobClient.GetContainerReference(WebConfigurationManager.AppSettings["AzureStorageAccountContainer"]);
+            return container;
+        }
+    }
+}
---
+++ AzureWebsite/PirCamWatcher/Models/PirCamBlobProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PirCamWatcher.Models
+{
+    public class PirCamBlobProperties
+    {
+        public DateTimeOffset? DateModified;
+
+        public Uri Uri;
+
+        public PirCamBlobProperties(DateTimeOffset? dateModified, Uri uri)
+        {
+            DateModified = dateModified; Uri = uri;
+        }
+    }
+}
